eabac45ef3e9619542f2acbf5e2fdb6f6eda5c45
Your Name <your.email@example.com>
2025-10-28 18:57:26 -0500

    ReorganizaciÃ³n: mover proyecto React de examples/ al directorio raÃ­z

--- examples/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './viewer-overrides-scoped.css';
-import './viewer-icons-toggle.js';
-import App from './App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('root'));
